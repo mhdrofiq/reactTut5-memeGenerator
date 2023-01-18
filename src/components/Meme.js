ba84d6b704df75c6a1831c7429c247c4fc62b378
Mhdrfq <mhdrfq.rahman@gmail.com>
2023-01-18 21:05:47 +0700

    apply modifying prev states and objects as states

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,23 @@ import React from "react"
 // import memesData from "../memesData.js"
 
 export default function Meme(){
-    
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        botText: "",
+        randomImage: "https://imgflip.com/s/meme/Laughing-Leo.png"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const randomNumbeer = Math.floor(Math.random() * memesArray.length)
+        const memesArray = allMemeImages.data.memes
+        const randomNumbeer = Math.floor(Math.random() * memesArray.length) 
         url = memesArray[randomNumbeer].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return(
@@ -30,6 +41,7 @@ export default function Meme(){
                     Get a new meme image
                 </button>
             </div>
+            <img src={meme.randomImage} className="meme-image"></img>
         </main>
     )
 }
